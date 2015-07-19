43d1d1c8d62a3c0ae69a84e0c6a372957e075658
yogesh kumar gupta <nmsyogesh@gmail.com>
2015-07-19 23:37:57 +0530

    Rmoving unneccessary files

--- src/org/sankalpnitjamshedpur/constants/LoginConstants.java
+++
@@ -1,9 +0,0 @@
-package org.sankalpnitjamshedpur.constants;
-
-public class LoginConstants {
-
-	public static final String KEY_VOLUNTEERID = "VolunteerId";
-	public static final String KEY_EMAILID = "EmailId";
-	public static final String KEY_MOBILENO = "MobileNo";
-	public static final String KEY_REGISTERED_TYPE = "RegisteredType";
-}
